ea6c829b2db58d396cdbeb5c30186c5ae1a76136
ReginaBik <reginabiktimirova1@gmail.com>
2022-06-22 19:06:43 -0500

    added etsy tests

---
+++ src/test/java/steps/EtsySteps.java
@@ -0,0 +1,18 @@
+package steps;
+
+import cucumber.api.java.en.Then;
+import io.cucumber.datatable.DataTable;
+
+public class EtsySteps {
+    @Then("user should see below menu item links")
+    public void user_should_see_below_menu_item_links(DataTable dataTable) {
+        // Write code here that turns the phrase above into concrete actions
+        // For automatic transformation, change DataTable to one of
+        // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+        // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+        // Double, Byte, Short, Long, BigInteger or BigDecimal.
+        //
+        // For other transformations you can register a DataTableType.
+        throw new cucumber.api.PendingException();
+    }
+}
